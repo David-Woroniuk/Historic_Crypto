d14589243203029a9b0b4f052ec2fca5956f712d
David <70294884+David-Woroniuk@users.noreply.github.com>
2020-11-24 23:01:42 +0000

    Update LiveCryptoData.py

--- LiveCryptoData.py
+++ LiveCryptoData.py
@@ -5,6 +5,7 @@ from pandas import json_normalize
 from datetime import date, datetime
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 from urllib.error import HTTPError
+from tqdm import tqdm
 
 class LiveCryptoData(object):
   '''
@@ -23,16 +24,28 @@ class LiveCryptoData(object):
 
   def __init__(self,
                ticker,
-               verbose = True):
+               verbose = False):
     
+    if verbose:
+      pbar = tqdm(total = 3)
+      pbar.set_description("Checking if string object")
+
     if isinstance(ticker, str) is False:
       raise TypeError("The 'ticker' argument must be a string object.")
+
+    if verbose:
+      pbar.update()
+      pbar.set_description("Checking if ticker available at Coinmarketcap.com")
 
     all_tickers = pd.read_html("https://coinmarketcap.com/all/views/all/")[-1]
     if ticker not in all_tickers['Symbol'].to_list():
       raise ValueError("""'{0}' was not found in the top 200 cryptocurrencies. \n
       Please search 'https://coinmarketcap.com/coins/' for the correct ticker. \n
       Alternatively, please run the 'find_all_tickers' function to return the appropriate ticker""".format(ticker))
+    
+    if verbose:
+      pbar.update()
+      self.pbar = pbar
     
     self.ticker = ticker
     self.verbose = verbose
@@ -42,6 +55,10 @@ class LiveCryptoData(object):
     '''
     This function retrieves the requested data.
     '''
+    if self.verbose:
+      self.pbar.set_description("Extracting market data from Coinmarketcap.com")
+      self.pbar.update()
+      
     for item in self.all_tickers['Symbol']:
       if item == self.ticker:
         price_data = self.all_tickers.loc[self.all_tickers['Symbol'] == item]
