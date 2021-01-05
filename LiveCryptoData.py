07cb5b345f330d7402fb6f943adb29e2cd446aaf
Leon Shams <52867365+LeonShams@users.noreply.github.com>
2021-01-05 01:33:53 -0800

    Use the user defined ticker

--- LiveCryptoData.py
+++ LiveCryptoData.py
@@ -65,7 +65,7 @@ class LiveCryptoData(object):
       self.pbar.update()
       self.pbar.set_description("Collecting data for '{}'".format(self.ticker))
 
-    response = requests.get("https://api.pro.coinbase.com/products/BTC-USD/ticker")
+    response = requests.get(f"https://api.pro.coinbase.com/products/{self.ticker}/ticker")
     if response.status_code == 200:
       response_dictionary= json.loads(response.text)
       data = pd.DataFrame.from_dict(response_dictionary, orient='index').T
