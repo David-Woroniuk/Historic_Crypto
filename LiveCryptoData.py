d6fb92330d3ba4c558420aedbfb8792cfa3835c8
David <70294884+David-Woroniuk@users.noreply.github.com>
2021-09-01 10:57:03 +0100

    Update LiveCryptoData.py

--- LiveCryptoData.py
+++ LiveCryptoData.py
@@ -1,79 +1,95 @@
 # -*- coding: utf-8 -*-
 
-!pip install requests
 import pandas as pd
 import requests
 import json
-from tqdm import tqdm_notebook as tqdm
+import sys
+
 
 class LiveCryptoData(object):
-  '''
-  This class provides a method for obtaining live Cryptocurrency price
-  data, including the bid/ask spread from the CoinBase Pro API.
-  -------------------------------Arguments-------------------------------------------
-   
-  ticker: information for which the user would like to return (str).
-  verbose: print progress during extraction, default = True (bool). (Default:True)
-  -------------------------------Returns---------------------------------------------  
-   
-  data: a Pandas DataFrame which contains the requested cryptocurrency data.
-  '''
+    """
+    This class provides methods for obtaining live Cryptocurrency price data,
+    including the Bid/Ask spread from the COinBase Pro API.
 
-  def __init__(self,
-               ticker,
-               verbose = True):
-    
-    if verbose:
-      pbar = tqdm(range(4), desc='tqdm 400px', ncols='1000px')
-      pbar.set_description("Checking if string object.")
+    :param: ticker: information for which the user would like to return. (str)
+    :param: verbose: print progress during extraction, default = True (bool)
+    :returns: response_data: a Pandas DataFrame which contains the requested cryptocurrency data. (pd.DataFrame)
+    """
+    def __init__(self,
+                 ticker,
+                 verbose=True):
 
-    if isinstance(ticker, str) is False:
-      raise TypeError("The 'ticker' argument must be a string object.")
+        if verbose:
+            print("Checking inputs...")
 
-    self.verbose = verbose
-    if verbose:
-      self.pbar = pbar
-    self.ticker = ticker
+        if not isinstance(ticker, str):
+            raise TypeError("The 'ticker' argument must be a string object.")
+        if not isinstance(verbose, (bool, type(None))):
+            raise TypeError("The 'verbose' argument must be a boolean or None type.")
 
-  def _ticker_checker(self):
-    '''This internal funciton checks if the ticker is available within the list of tickers.'''
-    if self.verbose:
-      self.pbar.update()
-      self.pbar.set_description("Checking if ticker supplied is available on the CoinBase Pro API.")
-    
-    ticker_response = requests.get("https://api.pro.coinbase.com/products")
-    if ticker_response.status_code == 200 and self.verbose:
-      self.pbar.update()
-      self.pbar.set_description('Connected to the CoinBase Pro API.')
-    elif ticker_response.status_code == 404 and self.verbose:
-      print('Error code 404, could not connect to the CoinBase API.')
-    
-    ticker_response_lists = json.loads(ticker_response.text)
-    ticker_data = pd.DataFrame(ticker_response_lists)
-    ticker_list = ticker_data["id"].tolist()
-    if self.ticker in ticker_list:
-      if self.verbose:
-        self.pbar.update()
-        self.pbar.set_description("Ticker '{0}' found at the CoinBase Pro API, continuing to extraction.".format(self.ticker))
-    else:
-      raise ValueError("""Ticker: '{0}' not available at the CoinBase Pro API. Please use the Cryptocurrencies class to identify the correct ticker.""".format(self.ticker))
+        self.verbose = verbose
+        self.ticker = ticker
 
-  def return_data(self):
-    '''This function returns the desired output.'''
-    if self.verbose:
-      self._ticker_checker()
-      self.pbar.update()
-      self.pbar.set_description("Collecting data for '{}'".format(self.ticker))
-    
-    response = requests.get(("https://api.pro.coinbase.com/products/{}/ticker").format(self.ticker))
-    if response.status_code == 200:
-      response_dictionary= json.loads(response.text)
-      data = pd.DataFrame.from_dict(response_dictionary, orient='index').T
-      data["time"] = pd.to_datetime(data["time"])
-      data.set_index("time", drop = True, inplace = True)
-    else:
-      raise ValueError("API Error: {}".format(response.status_code))
-    return data
-      
-new =  LiveCryptoData('ATOM-USD').return_data()
-new
+    def _ticker_checker(self):
+        """This helper function checks if the ticker is available on the CoinBase Pro API."""
+        if self.verbose:
+            print("Checking if user supplied is available on the CoinBase Pro API...")
+
+        tkr_response = requests.get("https://api.pro.coinbase.com/products")
+        if tkr_response.status_code in [200, 201, 202, 203, 204]:
+            if self.verbose:
+                print('Connected to the CoinBase Pro API.')
+            response_data = pd.json_normalize(json.loads(tkr_response.text))
+            ticker_list = response_data["id"].tolist()
+
+        elif tkr_response.status_code in [400, 401, 404]:
+            if self.verbose:
+                print("Status Code: {}, malformed request to the CoinBase Pro API.".format(tkr_response.status_code))
+            sys.exit()
+        elif tkr_response.status_code in [403, 500, 501]:
+            if self.verbose:
+                print("Status Code: {}, could not connect to the CoinBase Pro API.".format(tkr_response.status_code))
+            sys.exit()
+        else:
+            if self.verbose:
+                print("Status Code: {}, error in connecting to the CoinBase Pro API.".format(tkr_response.status_code))
+            sys.exit()
+
+        if self.ticker in ticker_list:
+            if self.verbose:
+                print("Ticker '{}' found at the CoinBase Pro API, continuing to extraction.".format(self.ticker))
+        else:
+            raise ValueError("""Ticker: '{}' not available through CoinBase Pro API. Please use the Cryptocurrencies 
+            class to identify the correct ticker.""".format(self.ticker))
+
+    def return_data(self):
+        """This function returns the desired output."""
+        if self.verbose:
+            print("Collecting data for '{}'".format(self.ticker))
+
+        self._ticker_checker()
+        response = requests.get("https://api.pro.coinbase.com/products/{}/ticker".format(self.ticker))
+
+        if response.status_code in [200, 201, 202, 203, 204]:
+            if self.verbose:
+                print("Status Code: {}, successful API call.".format(response.status_code))
+            response_data = pd.json_normalize(json.loads(response.text))
+            response_data["time"] = pd.to_datetime(response_data["time"])
+            response_data.set_index("time", drop=True, inplace=True)
+            return response_data
+        elif response.status_code in [400, 401, 404]:
+            if self.verbose:
+                print("Status Code: {}, malformed request to the CoinBase Pro API.".format(response.status_code))
+            sys.exit()
+        elif response.status_code in [403, 500, 501]:
+            if self.verbose:
+                print("Status Code: {}, error in connecting to the CoinBase Pro API.".format(response.status_code))
+            sys.exit()
+        else:
+            if self.verbose:
+                print("Status Code: {}, error in connecting to the CoinBase Pro API.".format(response.status_code))
+            sys.exit()
+
+
+new = LiveCryptoData('BTC-USD').return_data()
+print(new)
