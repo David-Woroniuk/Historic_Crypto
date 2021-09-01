e8a0dd72b96349dfffec9703e43eb321dc31be27
David <70294884+David-Woroniuk@users.noreply.github.com>
2021-09-01 09:09:42 +0100

    Update Cyptocurrencies.py
    code refactor, increased performance.

--- Cyptocurrencies.py
+++ Cyptocurrencies.py
@@ -1,76 +1,77 @@
 # -*- coding: utf-8 -*-
 
-!pip install requests
 import requests
 import json
-import time
-from random import randint
+import sys
 import pandas as pd
-from pandas import json_normalize
-from datetime import date, datetime, timedelta
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-from urllib.error import HTTPError
+
 
 class Cryptocurrencies(object):
-  '''
-  This class provides methods for finding all available Cryptocurrency
-  products within the CoinBase pro API.
+    """
+    This class provides methods for finding all available Cryptocurrency products within
+    the CoinBase pro API.
 
-  -------------------------------Arguments-------------------------------------------
+    :param: extended_output: displays either a condensed or extended output, Default = False (Bool).
+    :param: prints status messages Default = True (Bool) .
+    :param: search for a specific cryptocurrency string Default = None (str) .
+    :returns: a Pandas DataFrame containing either the extended or condensed output (DataFrame).
+    """
+    def __init__(self,
+                 extended_output=False,
+                 coin_search=None,
+                 verbose=True):
 
-  extended_output: displays either a condensed or extended output (Bool) (Default = False).
-  verbose: prints status messages (Bool) (Default = True).
-  coin_search: search for a specific cryptocurrency string (str) (Default = None).
+        if not all(isinstance(v, (bool, type(None))) for v in [extended_output, verbose]):
+            raise TypeError("The 'extended_output' and 'verbose' arguments must either be empty, or boolean types.")
+        if not isinstance(coin_search, (str, type(None))):
+            raise TypeError("The 'coin_search' argument must either be empty, or a string type.")
 
-  -------------------------------Returns--------------------------------------------- 
+        self.extended_output = extended_output
+        self.coin_search = coin_search
+        self.verbose = verbose
 
-  data: a Pandas DataFrame containing either the extended or condensed output (DataFrame).
-  '''
+    def find_crypto_pairs(self):
+        """This function returns all cryptocurrency pairs available at the CoinBase Pro API."""
+        response = requests.get("https://api.pro.coinbase.com/products")
+        if response.status_code in [200, 201, 202, 203, 204]:
+            if self.verbose:
+                print("Connected to the CoinBase Pro API.")
+            response_data = pd.json_normalize(json.loads(response.text))
+        elif response.status_code in [400, 401, 404]:
+            if self.verbose:
+                print("Status Code: {}, malformed request to the CoinBase Pro API.".format(response.status_code))
+            sys.exit()
+        elif response.status_code in [403, 500, 501]:
+            if self.verbose:
+                print("Status Code: {}, could not connect to the CoinBase Pro API.".format(response.status_code))
+            sys.exit()
+        else:
+            if self.verbose:
+                print("Status Code: {}, error in connecting to the CoinBase Pro API.".format(response.status_code))
+            sys.exit()
 
-  def __init__(self,
-               extended_output = False,
-               coin_search = None,
-               verbose = True):
-    
-    if coin_search is not None:
-      if isinstance(coin_search, str) is False:
-        raise TypeError("'coin_search' argument must either be empty, or a string type.")
-    
-    self.extended_output = extended_output
-    self.coin_search = coin_search
-    self.verbose = verbose
+        if self.coin_search:
+            outcome = response_data[response_data['id'].str.contains(self.coin_search)]
+            if not outcome.empty:
+                if self.verbose:
+                    print("Found {0} instances containing the term {1}.".format(outcome.shape[0], self.coin_search))
+            else:
+                outcome = response_data
+                if self.verbose:
+                    print("Unable to find specific search term, returning all available data.")
+        else:
+            outcome = response_data
 
-  def find_crypto_pairs(self):
-    '''
-    This function returns all cryptocurrency pairs available at the CoinBase Pro API.
-    '''
+        if self.extended_output:
+            if self.verbose:
+                print("Extended Output is {}, returning all data.".format(self.extended_output))
+            return outcome
+        else:
+            if self.verbose:
+                print("Extended Output is {}, returning reduced dataset.".format(self.extended_output))
+            outcome = outcome[['id', 'display_name', 'fx_stablecoin', 'max_slippage_percentage', 'status']]
+            return outcome
 
-    response = requests.get("https://api.pro.coinbase.com/products")
-    if response.status_code == 200 and self.verbose:
-      print('Connected to the CoinBase Pro API.')
-    elif response.status_code == 404 and self.verbose:
-      print('Error code 404, could not connect to the CoinBase API.')
-    
-    response_lists = json.loads(response.text)
-    data = pd.DataFrame(response_lists)
 
-    if self.coin_search != None:
-      outcome = data[data['id'].str.contains(self.coin_search)]
-      if outcome.empty == False:
-        if self.verbose:
-          print("Found {0} instances containing the term {1}.".format(outcome.shape[0],self.coin_search))
-      else:
-        if self.verbose:
-          print("Unable to find specific search term, returning all available terms.")
-
-    if self.extended_output:
-      return data
-    else:
-      data.drop(['base_currency','quote_currency','base_min_size','base_max_size','quote_increment',
-                'base_increment','min_market_funds','max_market_funds','margin_enabled','post_only',
-                'limit_only','cancel_only','trading_disabled','status_message'],axis = 1, inplace = True)
-      return data
-
-    
-data = Cryptocurrencies(coin_search = 'XLM', extended_output=False).find_crypto_pairs()
-data
+data = Cryptocurrencies(coin_search='DOGE', extended_output=False).find_crypto_pairs()
+print(data)
