0dbfc7ffc834450de01122c45740d6e92adb8fda
Nikhil Razab-Sekh <nikhilrazab-sekh@Nikhils-MacBook-Pro.local>
2024-02-08 15:59:57 +0100

    Modified script to output crypto pairs to CSV instead of printing

--- pairs.py
+++ pairs.py
@@ -4,5 +4,5 @@ from Cryptocurrencies import Cryptocurrencies
 crypto = Cryptocurrencies(extended_output=True)
 pairs = crypto.find_crypto_pairs()
 
-# Print the resulting DataFrame
-print(pairs)
+# Output the resulting DataFrame to a CSV file
+pairs.to_csv('crypto_pairs.csv', index=False)
